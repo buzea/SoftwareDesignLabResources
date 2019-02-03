c72dd39adc57491704171102af8dc1bf4ddee2fd
buzea <buzea.vlad@gmail.com>
2019-02-03 15:27:33 +0200

    JavaFx Example (not finished)

---
+++ Code Examples/JavaFxMVC/src/main/java/sample/Main.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/employee.fxml"));
+		Parent root = loader.load();
+		primaryStage.setTitle("Library");
+		primaryStage.setScene(new Scene(root, 300, 275));
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
