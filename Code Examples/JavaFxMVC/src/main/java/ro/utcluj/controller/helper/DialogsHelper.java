17eff6821b611ea903c2e825847acd3751355416
Vlad Buzea <vlad.buzea@itiviti.com>
2019-02-05 16:26:13 +0200

    Create Book and nicer layout
    Change-Id: Iaf0a7776b19a29c603658a75e97ad02d2f2db0fa

---
+++ Code Examples/JavaFxMVC/src/main/java/ro/utcluj/controller/helper/DialogsHelper.java
@@ -0,0 +1,49 @@
+/*************************************************************************
+ * ULLINK CONFIDENTIAL INFORMATION
+ * _______________________________
+ *
+ * All Rights Reserved.
+ *
+ * NOTICE: This file and its content are the property of Ullink. The
+ * information included has been classified as Confidential and may
+ * not be copied, modified, distributed, or otherwise disseminated, in
+ * whole or part, without the express written permission of Ullink.
+ ************************************************************************/
+package ro.utcluj.controller.helper;
+
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class DialogsHelper {
+
+    public static void showErrorDialog(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("An error occured");
+        alert.setContentText(message);
+
+        alert.showAndWait();
+    }
+
+    public static <R> Optional<R> showCustomDialog(String title, Node content, Supplier<R> dialogDataSupplier) {
+        Dialog<R> dialog = new Dialog<>();
+        dialog.setTitle(title);
+        dialog.getDialogPane().setContent(content);
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+        dialog.setResizable(true);
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton.getButtonData() == ButtonBar.ButtonData.OK_DONE) {
+                return dialogDataSupplier.get();
+            }
+            return null;
+        });
+        return dialog.showAndWait();
+    }
+}
